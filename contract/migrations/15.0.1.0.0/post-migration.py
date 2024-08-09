1024c5a19ab8c9e6676ab694417f6f121f6b379f
Emilio Pascual <emilio@moduon.team>
2024-08-09 07:54:30 +0200

    [FIX] contract: Problem when load data in migration
    @moduon MT-6161

--- contract/migrations/15.0.1.0.0/post-migration.py
+++ contract/migrations/15.0.1.0.0/post-migration.py
@@ -3,9 +3,9 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.load_data(
-        env.cr, "contract", "migrations/15.0.1.0.0/noupdate_changes.xml"
-    )
+    # openupgrade.load_data(
+    #     env.cr, "contract", "migrations/15.0.1.0.0/noupdate_changes.xml"
+    # )
     openupgrade.delete_record_translations(
         env.cr,
         "contract",
